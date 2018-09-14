2aa9620d901cceb262a0b0eeb7363aebc784198c
Rodrigo <rodrigomoura426@hotmail.com>
2018-09-14 18:35:05 +0000

    alteracao de permissoes recepcionista e terapeuta

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,20 +6,23 @@ class Ability
       if user.kind == 'therapist'
         can :access, :rails_admin
         can :dashboard
-        can :manage, Client
-        can :manage, PhoneClient
+        can :read, Client
+        can :read, PhoneClient
         can :read, PhoneUser
         can :manage, PhoneUser, user_id: user.id
         can :read, User
+        can :read, Address
         #can :manage, User, user_id: user.id
-        can :manage, Address
       elsif user.kind == 'manager'
         can :manage, :all
       elsif user.kind == 'receptionist'
         can :access, :rails_admin
         can :dashboard
         can :manage, Client
+        can :manage, PhoneClient
         can :manage, Address
+        can :manage, User
+        can :manage, PhoneUser
       end
     end
   end
