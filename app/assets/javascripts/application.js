a406c8d584cd470e76ad71bbdf35f9e0d4346349
Rodrigo <rodrigomoura426@hotmail.com>
2018-10-18 20:28:20 +0000

    applicaton.r

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -71,7 +71,7 @@ $(document).ready(function() {
       header: {
         left: 'prev,next today',
         center: 'title',
-        right: 'month,agendaWeek,agendaDay'
+        right: 'month,agendaWeek,agendaDay',
       },
       selectable: true,
       selectHelper: true,
