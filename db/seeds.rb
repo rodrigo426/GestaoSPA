bdb6ca6bc11882f709a5e96ff30695cdeb5e2d4f
Rodrigo <rodrigomoura426@hotmail.com>
2018-11-18 01:21:47 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # /db/seeds.rb
 
-User.create name: 'Julio', status: :active, kind: :manager, email: 'julio@teste.com', password: 123456
-User.create name: 'Manuel', status: :active, kind: :therapist, email: 'manuel@teste.com', password: 123456
-User.create name: 'Marcos', status: :active, kind: :therapist, email: 'marcos@teste.com', password: 123456
+User.create name: 'Julio', status: :active, kind: :gerente, email: 'julio@teste.com', password: 123456
+User.create name: 'Manuel', status: :active, kind: :terapeuta, email: 'manuel@teste.com', password: 123456
+User.create name: 'Marcos', status: :active, kind: :terapeuta, email: 'marcos@teste.com', password: 123456
 
 
 
