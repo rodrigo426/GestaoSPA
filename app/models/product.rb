e6e3f8fea1f0c4b12a481992db7904af524542ae
Rodrigo <rodrigomoura426@hotmail.com>
2019-01-29 18:04:49 +0000

    views de produtos e permissoes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
 	has_many :product_movements
 	#validates_numericality_of :quantity, :only_integer => true, :greater_than_or_equal_to => 0
+
+  def name_with_price
+    "#{name}: R$#{price}"
+  end
 end
