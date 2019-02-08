a43b79ab6f0cbcd98eea3b3683b62f4ddc3481b9
Rodrigo <rodrigomoura426@hotmail.com>
2019-02-08 00:10:17 +0000

    migration

--- db/migrate/20190207200458_mudar_nome_repasse.rb
+++
@@ -1,9 +0,0 @@
-class MudarNomeRepasse < ActiveRecord::Migration[5.2]
-  def self.up
-    rename_column :cashiers, :repasse, :therapist_value
-  end
-
-  def self.down
-    rename_column :cashiers, :repasse, :therapist_value
-  end
-end
