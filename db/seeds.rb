7fb7ca1b1407d201b577d6a17267c662b80255f8
Rodrigo <rodrigomoura426@hotmail.com>
2019-03-28 23:53:24 +0000

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 # /db/seeds.rb
 
 #User.create name: 'Julio', status: :active, kind: :gerente, email: 'julio@teste.com', password: 123456
-#User.create name: 'Rodrigo', status: :active, kind: :tecnico, email: 'rodrigomoura426@gmail.com', password: 'umv6krc2ze'
+#User.create name: 'Rodrigo', status: :active, kind: :tecnico, email: 'rodrigomoura426@gmail.com', password: '123456'
 #User.create name: 'Manuel', status: :active, kind: :terapeuta, email: 'manuel@teste.com', password: 123456
 #User.create name: 'Marcos', status: :active, kind: :terapeuta, email: 'marcos@teste.com', password: 123456
 
 
 
-#Client.create name: 'Vitor Ganiko', birthdate: 'June 9, 1997', email: 'japao@teste.com', occupation: 'pasteleiro'
-#Client.create name: 'Vitor Festine', birthdate: 'September 9, 1997', email: 'vitao@teste.com', occupation: 'chefe do gordao'
+#Client.create name: 'Vitor Ganiko', birthdate: 'June 9, 1997', email: 'vsganiko@teste.com', occupation: 'empresario'
+#Client.create name: 'Vitor Festine', birthdate: 'September 9, 1997', email: 'vitao@teste.com', occupation: 'gerente'
 #Client.create name: 'Rodrigo Moura', birthdate: 'December 22, 1995', email: 'rodrigo@teste.com', occupation: 'estudante'
-#Client.create name: 'Kathleen Saraiva', birthdate: 'June 19, 1997', email: 'saraiva@teste.com', occupation: 'trabalha na google'
+#Client.create name: 'Kathleen Saraiva', birthdate: 'June 19, 1997', email: 'saraiva@teste.com', occupation: 'estudante'
 
 
 #Therapy.create name: 'Acupuntura', price: '200.00'
