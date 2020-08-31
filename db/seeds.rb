0ce39f58792230d7a97949b263d482c09becf3a4
Rodrigo Moura <rodrigomoura426@hotmail.com>
2020-08-31 02:59:24 +0000

    terapias inativas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # /db/seeds.rb
 
 #User.create name: 'Julio', status: :active, kind: :gerente, email: 'julio@teste.com', password: 123456
-#User.create name: 'Rodrigo', status: :active, kind: :tecnico, email: 'rodrigomoura426@gmail.com', password: '123456'
+User.create name: 'Rodrigo', status: :active, kind: :tecnico, email: 'rodrigomoura426@gmail.com', password: '123456'
 #User.create name: 'Manuel', status: :active, kind: :terapeuta, email: 'manuel@teste.com', password: 123456
 #User.create name: 'Marcos', status: :active, kind: :terapeuta, email: 'marcos@teste.com', password: 123456
 
@@ -13,9 +13,9 @@
 #Client.create name: 'Kathleen Saraiva', birthdate: 'June 19, 1997', email: 'saraiva@teste.com', occupation: 'estudante'
 
 
-#Therapy.create name: 'Acupuntura', price: '200.00'
-#Therapy.create name: 'Shiatshu', price: '250.00'
-#Therapy.create name: 'Quick Massage', price: '180.00'
+Therapy.create name: 'Acupuntura', price: '200.00'
+Therapy.create name: 'Shiatshu', price: '250.00'
+Therapy.create name: 'Quick Massage', price: '180.00'
 
 
 
